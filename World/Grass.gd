6d2d1c2c386409e244c0046d61145badfe6dff61
denvermullets <devmullets@gmail.com>
2021-07-29 07:01:48 -0400

    ep15, refactor death effects and apply to enemies and grass

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_effect():
 #	basically you need to add the effect to the DOM 
 #	kinda unsure how the global_position works, will reasearch later
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grassEffect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(grassEffect)
+	get_parent().add_child(grassEffect)
 	grassEffect.global_position = global_position
 
 func _on_Hurtbox_area_entered(area):
